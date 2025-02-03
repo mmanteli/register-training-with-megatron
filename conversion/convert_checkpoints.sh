18ce596df7840038c60ddb8c88780e4a054a75b9
mmanteli <amanda.myntti@gmail.com>
2025-02-03 11:58:35 +0200

    Conversion scripts working now

--- conversion/convert_checkpoints.sh
+++ conversion/convert_checkpoints.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=7
 #SBATCH --mem=50G
-#SBATCH --partition=debug
-#SBATCH --time=00:30:00
+#SBATCH --partition=small
+#SBATCH --time=01:29:00
 #SBATCH --account=project_462000615
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
 
 
-# this contains dification for output head (related to 1.71B vs 1.82B difference)
 register=$1
+limit="0015000"  # limiting to this for now
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: input register as parameter"
@@ -20,7 +20,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 
-echo "CONVERTING ${REGISTER} CHECKPOINTS"
+echo "CONVERTING ${register} CHECKPOINTS"
 
 module purge
 module use /appl/local/csc/modulefiles/
@@ -40,33 +40,37 @@ for checkpoint in $ckpt/$register/*; do
         continue
     fi
 
+    # skip if iteration number is over the limit:
+    iteration=$(echo $checkpoint | rev | cut -f 1 -d"_" | rev )  # iteration number is the last field separated by _
+    if (( 10#$iteration > 10#$limit )); then
+        echo "skipping ${iteration} due to limit"
+        continue 1
+    fi
+
     b=$(basename $checkpoint)
     output="${o}/${register}/${b}"
     #echo $output
     mkdir -p $output
 
     # see if we have already converted this
-    FILE_COUNT=$(find "$output" -maxdepth 1 -type f -name "*.safetensors" | wc -l)
-    if (( FILE_COUNT >= 2 )); then
-        echo "Directory exists and contains at least two .safetensors files, no recalculation."
+    FILE_COUNT=$(find "$output" -maxdepth 1 -type f -name "*model.bin" | wc -l)
+    if (( FILE_COUNT >= 1 )); then
+        echo "Directory iter_${iteration} exists and contains model.bin, no recalculation."
         continue 1
     fi
     srun python3 ${eval_path}/tools/checkpoint/util.py \
                     --model-type GPT \
                     --loader loader_megatron \
                     --saver saver_llama2_hf \
-                    --tokenizer-dir gpt2-tokenizer \
+                    --tokenizer-dir gpt2-tokenizer/ \
                     --load-dir "$checkpoint" \
                     --save-dir "$output"
+    echo " "
+    echo "-----------------------------------------------------------------"
+    echo " "
 
-    echo "FIRST DONE; EXITING"
-    exit
 done
 
-#srun python3 ${eval_path}/tools/checkpoint/util.py \
-#                    --model-type GPT \
-#                    --loader loader_megatron \
-#                    --saver saver_llama2_hf \
-#                    --tokenizer-dir gpt2-tokenizer \
-#                    --load-dir "$ckpt" \
-#                    --save-dir "$o"
+mkdir -p logs/done
+cp logs/${SLURM_JOB_NAME}-${SLURM_JOB_ID}.err logs/done/${register}_${SLURM_JOB_NAME}-${SLURM_JOB_ID}.err
+cp logs/${SLURM_JOB_NAME}-${SLURM_JOB_ID}.out logs/done/${register}_${SLURM_JOB_NAME}-${SLURM_JOB_ID}.out
---
+++ conversion/gpt2-tokenizer/README.md
@@ -0,0 +1,9 @@
+This directory intentionally left empty. It should contain
+
+- merges.txt
+- special_tokens_map.json
+- tokenizer_config.json
+- tokenizer.json
+- vocab.json
+
+for the selected tokenizer.
