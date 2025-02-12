1b7e68bf6d4986706c02705991e83b9873deb3f7
mmanteli <amanda.myntti@gmail.com>
2025-02-12 10:41:11 +0200

    Added skipping for existing results

--- evaluation/run_evaluate.sh
+++ evaluation/run_evaluate.sh
@@ -8,5 +8,11 @@ ckpts="/scratch/project_462000353/amanda/megatron-training/register-training-wit
 
 for dir in $ckpts/*; do
     iteration_number=$(echo $dir | rev | cut -f 1 -d"_" | rev )
+    already_evaluated=$(find eval_results/${evaluation}/${REGISTER}/${REGISTER}_iter_${iteration_number} -maxdepth 1 -type f -name "*.json" |wc -l)
+    if (( already_evaluated >= 1 )); then
+        echo "Directory ${REGISTER}_iter_${iteration_number} exists and contains results, no recalculation."
+        continue 1
+    fi
     sbatch evaluate.sh $evaluation $REGISTER $iteration_number 
+    #sleep 2
 done
